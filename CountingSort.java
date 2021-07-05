5ac40d66a279dca5cd8312946c9bb8cd05bfa0b2
vishnuk7 <vishnu.kraman88@gmail.com>
2021-07-05 08:27:47 +0530

    Counting Sort 🔟

---
+++ CountingSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class CountingSortSol {
+    public void sort(int[] array) {
+        int max = Arrays.stream(array).max().getAsInt();
+        sort(array, max);
+    }
+
+    private void sort(int[] array, int max) {
+        int[] counts = new int[max + 1];
+
+        for (var item : array)
+            counts[item]++;
+
+        int k = 0;
+        for (int i = 0; i < counts.length; i++) {
+            for (int j = 0; j < counts[i]; j++) {
+                array[k++] = i;
+            }
+        }
+
+    }
+}
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int nums[] = { 5, 1, 7, 2, 6, 4, 1 };
+        new CountingSortSol().sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
